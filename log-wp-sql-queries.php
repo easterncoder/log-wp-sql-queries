ff077368bae29473f54fdc1723b70c0bb5414380
Mike Lopez <mike@wishlistproducts.com>
2021-08-26 02:03:31 +0800

    changed log format

--- log-wp-sql-queries.php
+++ log-wp-sql-queries.php
@@ -21,7 +21,8 @@ add_action( 'shutdown', '\log_wp_sql_queries\log_queries' );
  */
 function log_queries() {
 	global $wpdb;
+	$session = md5( microtime() );
 	foreach ( $wpdb->queries as $q ) {
-		error_log( sprintf( 'WP SQL Query Log: %1$s - (%2$s s) [Stack]: %3$s', ...array_values( $q ) ) );
+		error_log( sprintf( 'WP SQL Query Log (%1$s): %2$s - (%3$s s) [Stack]: %4$s', $session, preg_replace( '/[\r\n]/', '', $q[0] ), $q[1], $q[2] ) );
 	}
 }
